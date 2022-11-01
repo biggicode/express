17b66b3be1cb79dd58b2c8154fa87c50cfca490f
Oncescu Laurentiu <larioncescu@yahoo.com>
2022-11-01 21:15:44 +0200

    make basic server

--- app.js
+++ app.js
@@ -2,9 +2,22 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log("ala bala mere");
-  res.writeHead(200, { "content-type": "text/html" });
-  res.write("<h1>home page</h1>");
-  res.end();
+  const url = req.url;
+
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>home page</h1>");
+    res.end();
+  } else if (url === "/about") {
+    //about page
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>about page</h1>");
+    res.end();
+  } else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("<h1>page not found</h1>");
+    res.end();
+  }
 });
 
 server.listen(5000);
