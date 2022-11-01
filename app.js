c5bda36ccdb0498875c63570e2d038de1f0964d1
Oncescu Laurentiu <larioncescu@yahoo.com>
2022-11-01 18:47:17 +0200

    http basics

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("hellow world");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log("ala bala mere");
+  res.end("home page");
+});
+
+server.listen(5000);
