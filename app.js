a6ef12b730167267f0173225bb1abbe552ae5aef
Oncescu Laurentiu <larioncescu@yahoo.com>
2022-11-03 20:40:45 +0200

    add static assets

--- app.js
+++ app.js
@@ -1,26 +1,19 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).send("home page");
-});
+//set up static and middleware
+app.use(express.static("./public"));
 
-app.get("/about", (req, res) => {
-  res.status(200).send("About page");
+app.get("/", (req, res) => {
+  res.status(200).sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
 });
 
 app.all("*", (req, res) => {
-  res.status(404).send("<h1> resource not found </h1>");
+  res.status(404).send("cannot find the resource");
 });
 
 app.listen(5000, () => {
   console.log("server is listening on port 5000");
 });
-
-// app.get
-// app.post
-// app.put
-// app.delete
-// app.all
-// app.use
-// app.listen
