2cb866eaa2fe90d8d9b110847ecde2b6d4b1d264
Oncescu Laurentiu <larioncescu@yahoo.com>
2022-11-05 22:17:58 +0200

    introduction to params in express

--- app.js
+++ app.js
@@ -2,8 +2,29 @@ const express = require("express");
 const app = express();
 const { products } = require("./data");
 
-app.get("*", (req, res) => {
-  res.json(products);
+app.get("/", (req, res) => {
+  res.send('<h1>Home Page</h1><a href="/api/products">products</a>');
+});
+
+// app.get("/api/products", (req, res) => {
+//   const newProducts = products.map((product) => {
+//     const { id, name, image } = product;
+//     return { id, name, image };
+//   });
+
+//   res.json(newProducts);
+// });
+app.get("/api/products/:productID", (req, res) => {
+  // console.log(req.params);
+  const { productID } = req.params;
+  const oneProduct = products.find(
+    (product) => product.id === Number(productID)
+  );
+  if (!oneProduct) {
+    return res.status(404).send("Product Does Not Exist");
+  }
+
+  res.json(oneProduct);
 });
 
 app.listen(5000, () => {
