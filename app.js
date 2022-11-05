f5cd6263a0033ab8d0034fb3790745335c17ee39
Oncescu Laurentiu <larioncescu@yahoo.com>
2022-11-05 22:50:09 +0200

    query params

--- app.js
+++ app.js
@@ -27,6 +27,29 @@ app.get("/api/products/:productID", (req, res) => {
   res.json(oneProduct);
 });
 
+app.get("/api/v1/query", (req, res) => {
+  // console.log(req.query);
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search);
+    });
+  }
+
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit));
+  }
+
+  if (sortedProducts.length < 1) {
+    // res.status(200).send("no products matched your search");
+    return res.status(200).json({ succes: true, data: [] });
+  }
+
+  res.status(200).json(sortedProducts);
+});
+
 app.listen(5000, () => {
   console.log("Server is listening on port 5000");
 });
