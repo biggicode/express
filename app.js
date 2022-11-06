1a9dfe7eb095693c4606b7e9684ad73f7bd50259
Oncescu Laurentiu <larioncescu@yahoo.com>
2022-11-06 12:08:55 +0200

    app use

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 const express = require("express");
 const app = express();
+const logger = require("./logger");
 
 // req => middleware => res
+app.use("/api", logger);
+// api/home/about/products
 
-const logger = (req, res, next) => {
-  const method = req.method;
-  const url = req.url;
-  const time = new Date().getFullYear();
-  console.log(method, url, time);
-  // res.send("Testing");
-  next();
-};
-
-app.get("/", logger, (req, res) => {
+app.get("/", (req, res) => {
   res.send("Home");
 });
 
-app.get("/about", logger, (req, res) => {
+app.get("/about", (req, res) => {
   res.status(200).send("About");
+});
+
+app.get("/api/items", (req, res) => {
+  res.send("Items");
+});
+
+app.get("/api/products", (req, res) => {
+  res.status(200).send("Products");
 });
 
 app.listen(5000, () => {
