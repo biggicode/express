4781067fd4b991096fac90864b9ecd2661ce96b8
Oncescu Laurentiu <larioncescu@yahoo.com>
2022-11-08 21:53:01 +0200

    install postman

--- app.js
+++ app.js
@@ -24,6 +24,16 @@ app.post("/api/people", (req, res) => {
   res.status(201).json({ succes: true, person: name });
 });
 
+app.post("/api/postman/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ succes: false, msg: "please provide name value lalala" });
+  }
+  res.status(201).send({ succes: true, data: [...people, name] });
+});
+
 app.post("/login", (req, res) => {
   const { name } = req.body;
   if (name) {
