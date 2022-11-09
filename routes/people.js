fc3ed34f67e0d234cd667db15eec14c7867dddd6
Oncescu Laurentiu <larioncescu@yahoo.com>
2022-11-09 21:34:28 +0200

    add controllers

--- routes/people.js
+++ routes/people.js
@@ -1,67 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
-let { people } = require("../data");
+const {
+  getPeople,
+  createPerson,
+  createPersonPostman,
+  updatePerson,
+  deletePerson,
+} = require("../controllers/people");
 
-router.get("/", (req, res) => {
-  res.status(200).json({ succes: true, data: people });
-});
+// router.get("/", getPeople);
+// router.post("/", createPerson);
+// router.post("/postman", createPersonPostman);
+// router.put("/:id", updatePerson);
+// router.delete("/:id", deletePerson);
 
-router.post("/", (req, res) => {
-  const { name } = req.body;
-  if (!name) {
-    return res
-      .status(400)
-      .json({ succes: false, msg: "Please provide name value" });
-  }
-  res.status(201).json({ succes: true, person: name });
-});
-
-router.post("/postman", (req, res) => {
-  const { name } = req.body;
-  if (!name) {
-    return res
-      .status(400)
-      .json({ succes: false, msg: "please provide name value lalala" });
-  }
-  res.status(201).send({ succes: true, data: [...people, name] });
-});
-
-router.put("/:id", (req, res) => {
-  const { id } = req.params;
-  const { name } = req.body;
-
-  const person = people.find((person) => person.id === Number(id));
-
-  if (!person) {
-    return res
-      .status(400)
-      .json({ succes: false, msg: "can not find the person" });
-  }
-  const newPeople = people.map((person) => {
-    if (person.id === Number(id)) {
-      person.name = name;
-    }
-    return person;
-  });
-
-  res.status(200).json({ succes: true, data: newPeople });
-});
-
-router.delete("/:id", (req, res) => {
-  const { id } = req.params;
-
-  const person = people.find((person) => person.id === Number(id));
-
-  if (!person) {
-    return res
-      .status(400)
-      .json({ succes: false, msg: "can not find the person" });
-  }
-
-  const newPeople = people.filter((person) => person.id !== Number(id));
-
-  return res.status(200).json({ succes: true, data: newPeople });
-});
+router.route("/").get(getPeople).post(createPerson);
+router.route("/postman").post(createPersonPostman);
+router.route("/:id").put(updatePerson).delete(deletePerson);
 
 module.exports = router;
